c0e6945668e482cf82371fbd10d9a4ce701d4462
Jean-Christophe GAY <contact@jeanchristophegay.com>
2015-12-30 19:22:05 +0100

    Run git command

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/codegangsta/cli"
 	"io"
 	"os"
@@ -25,23 +27,80 @@ func (repos *RegisteredRepositories) List() []string {
 	return []string{"/Users/jcgay/dev/maven-notifier", "/Users/jcgay/dev/maven-color"}
 }
 
-func run(args []string, output io.Writer, error io.Writer, repos Repositories) {
-	for _, repo := range repos.List() {
-		command := exec.Command("echo", repo)
+type RunnableCommand interface {
+	Executable() string
+	Options() []string
+}
+
+type GitPullCommand struct{}
+
+func (command *GitPullCommand) Executable() string {
+	return "git"
+}
+
+func (command *GitPullCommand) Options() []string {
+	return []string{"pull", "--rebase=preserve"}
+}
+
+type GitShowCurrentBranchCommand struct{}
+
+func (command *GitShowCurrentBranchCommand) Executable() string {
+	return "git"
+}
+
+func (command *GitShowCurrentBranchCommand) Options() []string {
+	return []string{"symbolic-ref", "--short", "HEAD"}
+}
+
+type EchoCommand struct{}
+
+func (command *EchoCommand) Executable() string {
+	return "pwd"
+}
+
+func (command *EchoCommand) Options() []string {
+	return []string{}
+}
+
+type Runner struct {
+	RunnableCommand
+
+	repos  Repositories
+	writer io.Writer
+}
+
+func NewRunner(command RunnableCommand) *Runner {
+	return &Runner{
+		RunnableCommand: command,
+		repos:           &RegisteredRepositories{},
+		writer:          os.Stdout,
+	}
+}
+
+func (runner *Runner) Run() {
+	for _, repo := range runner.repos.List() {
+		output := new(bytes.Buffer)
+		errorOutput := new(bytes.Buffer)
+
+		command := exec.Command(runner.RunnableCommand.Executable(), runner.RunnableCommand.Options()...)
 		command.Stdout = output
-		command.Stderr = error
+		command.Stderr = errorOutput
 		command.Dir = repo
 		command.Run()
+
+		fmt.Fprint(runner.writer, output.String())
 	}
 }
 
 func buildCommands() []cli.Command {
 	commands := make([]cli.Command, 1)
-	commands = append(commands, cli.Command{
-		Name: "echo",
-		Action: func(context *cli.Context) {
-			run(os.Args, os.Stdout, os.Stderr, &RegisteredRepositories{})
-		},
-	})
+	commands = append(commands, cli.Command{Name: "echo", Action: func(context *cli.Context) {
+		NewRunner(&EchoCommand{}).Run()
+	},
+	}, cli.Command{Name: "pull", Action: func(context *cli.Context) {
+		NewRunner(&GitPullCommand{}).Run()
+	}}, cli.Command{Name: "current-branch", Action: func(context *cli.Context) {
+		NewRunner(&GitShowCurrentBranchCommand{}).Run()
+	}})
 	return commands
 }
